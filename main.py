c54051cd568046b28f707bf5e8fae84f3459cc50
fathimasilviyak <silviyakvm@gmail.com>
2023-03-26 21:19:40 +0530

    another method to open file using 'with' keyword

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-file = open("my_file.txt")
-contents = file.read()
-print(contents)
-file.close()
+# file = open("my_file.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
+
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents)

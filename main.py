aa001fbad1053e3061ce34575caa90452c8b4c41
fathimasilviyak <silviyakvm@gmail.com>
2023-03-26 21:36:06 +0530

    write to a new file

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+#Read file
 # file = open("my_file.txt")
 # contents = file.read()
 # print(contents)
 # file.close()
 
-with open("my_file.txt") as file:
-    contents = file.read()
-    print(contents)
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# Write to file
+with open("my_file.txt", mode="w") as file:
+    file.write("Hello world!")

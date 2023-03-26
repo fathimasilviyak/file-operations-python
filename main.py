34da0c6ef0bead3ec3f8ad95c6b4aafc42aed214
fathimasilviyak <silviyakvm@gmail.com>
2023-03-26 21:40:12 +0530

    append data to existing file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Read file
+# Read file
 # file = open("my_file.txt")
 # contents = file.read()
 # print(contents)
@@ -9,5 +9,9 @@
 #     print(contents)
 
 # Write to file
-with open("my_file.txt", mode="w") as file:
-    file.write("Hello world!")
+# with open("my_file.txt", mode="w") as file:
+#     file.write("Hello world!")
+#
+# Append data to existing file
+with open("my_file.txt", mode="a") as file:
+    file.write("\nI also like ice cream.")

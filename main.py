3ba7a2bd910e1e7d3e5523ee6e85f87ed5781b94
fathimasilviyak <silviyakvm@gmail.com>
2023-03-27 14:49:40 +0530

    absolute file path and relative file path

--- main.py
+++ main.py
@@ -4,9 +4,22 @@
 # print(contents)
 # file.close()
 
+# Another method to open file, which doesn't need to separately close the file
 # with open("my_file.txt") as file:
 #     contents = file.read()
 #     print(contents)
+
+# # Absolute file path
+# with open("C:/Users/silviya/Desktop/python/project/test.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+
+# Relative file path
+with open("../test.txt") as file:
+    contents = file.read()
+    print(contents)
+
 
 # Write to file
 # with open("my_file.txt", mode="w") as file:
@@ -17,7 +30,5 @@
 #     file.write("\nI also like ice cream.")
 
 # Write to non-existent file
-with open("new_file.txt", mode="w") as file:
-    file.write("This is a new file")
-
-
+# with open("new_file.txt", mode="w") as file:
+#     file.write("This is a new file")
